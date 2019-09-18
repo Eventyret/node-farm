31f8dfe007ab10235c89824ac734a869d0eaacc7
Eventyret <simen.dehlin@gmail.com>
2019-09-18 18:47:32 +0100

    Added dummy 404 page and testing pathing

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World');
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('Welcome to the Overview Page');
+  } else if (pathName === 'product') {
+    res.end('This is the product page');
+  } else {
+    res.writeHead(404)
+    res.end('Page could not be found');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
