425fd5622bbc397017c64139ec170561a9f1b5af
Eventyret <simen.dehlin@gmail.com>
2019-09-18 19:20:04 +0100

    Simple api created now has /api

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/' || pathName === '/overview') {
@@ -9,13 +12,10 @@ const server = http.createServer((req, res) => {
   } else if (pathName === 'product') {
     res.end('This is the product page');
   } else if (pathName === '/api') {
-    fs.readFile(`${__dirname}/data/data.json`, 'utf-8', (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, {
-        'Content-type': 'application/json'
-      });
-      res.end(data);
+    res.writeHead(200, {
+      'Content-type': 'application/json'
     });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html'
